41bbd4f2801a19b764cb5c0dee78e740035fc122
Celeste Aguilar <111248716+MadMax2808@users.noreply.github.com>
2025-04-06 15:47:41 -0600

    correccion errores 2

--- next.config.ts
+++ next.config.ts
@@ -3,14 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   async redirects() {
     return [
-      // Comentado para probar sin la redirección
-      
       {
         source: "/",
         destination: "/conversations",
         permanent: true,
       },
-      
     ];
   },
 };
